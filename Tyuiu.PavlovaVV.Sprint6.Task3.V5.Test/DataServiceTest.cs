d46225fd7df79dd38342d9bc5d9a86121fd80fb4
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-12-10 17:41:36 +0500

    Task3

--- Tyuiu.PavlovaVV.Sprint6.Task3.V5.Test/DataServiceTest.cs
+++ Tyuiu.PavlovaVV.Sprint6.Task3.V5.Test/DataServiceTest.cs
@@ -16,7 +16,7 @@ namespace Tyuiu.PavlovaVV.Sprint6.Task3.V5.Test
             int[,] res = ds.Calculate(matrix);
             int[,] wait = new int[5, 5] {   { 14, 1, -3, 1, 10 },
                                             { 7, -3, 5, -4, 0 },
-                                            { 0, -19, 0, -9, 19 },
+                                            { -10, -19, -18, -9, 19 },
                                             { -2, -2, -16, 2, -17 },
                                             { -16, 9, 5, -10, 16 } };
             CollectionAssert.AreEqual(wait, res);
